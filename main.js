aa33598cca5d8729ae9203fe5c8699c5e3e038f0
wende2006 <132565505+wende2006@users.noreply.github.com>
2024-03-25 19:24:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-05-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-08-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-08-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-09-22T00:00:00");
+const tempoObjetivo1 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo2 = new Date("2027-08-05T00:00:00");
+const tempoObjetivo3 = new Date("2030-05-30T00:00:00");
+const tempoObjetivo4 = new Date("2025-12-22T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
